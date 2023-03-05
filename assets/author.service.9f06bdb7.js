50621185403aedc29f5381a4045d9edf288f3d4e
Tran Van Sang <96764199+tran-van-sang@users.noreply.github.com>
2023-03-05 16:06:47 +0700

    Add files via upload

---
+++ assets/author.service.9f06bdb7.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index.54165c96.js";class r{constructor(t="/api/authors"){this.api=a(t)}async getAllAuthors(){return(await this.api.get("/")).data}async getAnAuthor(t){return(await this.api.get(`/${t}`)).data}async addAnAuthor(t){return(await this.api.post("/",t)).data}}const s=new r;export{s as A};
